22e3faeb6f9efec84c7f0bba2439bd6b9ae5c49b
4321iva <162304549+4321iva@users.noreply.github.com>
2024-10-11 08:52:53 +0300

    commit

---
+++ 2/2/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isPrime = IsPrime(number);
+
+        if (isPrime)
+        {
+            Console.WriteLine($"{number} є простим числом.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} не є простим числом.");
+        }
+    }
+
+    // Функція для перевірки, чи є число простим
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
